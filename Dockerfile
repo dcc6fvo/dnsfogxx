6be9fed27f86d1b84fe38549b8b86a95fe25d882
Felipe volpato <fvolpato@gmail.com>
2022-05-17 10:51:28 -0300

    Segunda versao

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 # syntax=docker/dockerfile:1
 FROM resystit/bind9:latest
 MAINTAINER Felipe Volpato 
-ARG rede
-ENV env_rede=$rede
 WORKDIR /etc/bind
-COPY XX.16.172.in-addr.arpa /etc/bind/${env_rede}.16.172.in-addr.arpa
-COPY XX-db.cti /etc/bind/db.cti
-RUN sed -i "s/XX/$env_rede/g" db.cti  
-COPY XX-named.conf /etc/bind/named.conf
-RUN sed -i "s/XX/$env_rede/g" named.conf  
+
+ARG zone network
+ENV env_zone=$zone env_net=$network
+
+COPY ns/ZZ.YY.XX.in-addr.arpa /tmp/ZZ.YY.XX.in-addr.arpa
+COPY ns/db.zone /tmp/db.zone
+COPY ns/named.conf /tmp/named.conf
+COPY ns/do-it-all.sh /tmp/do-it-all.sh
+
+RUN chmod +x /tmp/do-it-all.sh
+RUN /bin/sh /tmp/do-it-all.sh ${env_net} ${env_zone}
+
 EXPOSE 53/udp 
---
+++ ns/do-it-all.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+network=""
+zone=""
+
+ if [ "$#" -eq  "2" ]
+  then
+    echo "Starting script.."
+    echo "Network is $1"
+    echo "Zone is $2"
+    network=$1
+    zone=$2
+ else
+    echo "Error with the arguments. You need to set network and zone args."
+    exit 0;
+ fi
+
+do_reverse()
+{
+ dots=$(echo $1 | grep -o "\." | wc -l)
+
+ if [ "$dots" -eq  "3" ]
+  then
+     echo $1 | awk -F. '{print $4"."$3"." $2"."$1}'
+  else
+     echo $1 | awk -F. '{print $3"."$2"."$1}'
+ fi
+}
+
+
+do_changes()
+{
+ mv /tmp/ZZ.YY.XX.in-addr.arpa /etc/bind/$1.in-addr-arpa
+ sed -i "s/zzzz/$2/g" /etc/bind/$1.in-addr-arpa
+ 
+ mv /tmp/db.zone /etc/bind/db.$2
+ sed -i "s/zzzz/$2/g" /etc/bind/db.$2
+ sed -i "s/XX.YY.ZZ/$3/g" /etc/bind/db.$2
+ 
+ mv /tmp/named.conf /etc/bind/named.conf
+ sed -i "s/zzzz/$2/g" /etc/bind/named.conf
+ sed -i "s/ZZ.YY.XX/$1/g" /etc/bind/named.conf
+}
+
+reverse=$(do_reverse $1);
+do_changes $reverse $zone $network
+
